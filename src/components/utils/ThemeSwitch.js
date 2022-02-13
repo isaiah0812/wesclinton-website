2e82deea666da768c7371c81ba2d266c808e296a
Isaiah Bullard <isaiah0812@yahoo.com>
2022-02-13 13:15:03 -0600

    add basic design for theme switch

--- src/components/utils/ThemeSwitch.js
+++ src/components/utils/ThemeSwitch.js
@@ -8,10 +8,6 @@ export const ThemeSwitch = () => {
   return (
     <Form.Switch
       id="custom-switch"
-      style={{
-        width: '3.1em',
-        height: '3.1em',
-      }}
       onChange={() => switchTheme()}
       checked={ theme.id === 'wes-clinton' }
     />
